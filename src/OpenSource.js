8d277292b463c5dc01303436b26f48e25bf32319
Diana Mounter <broccolini@github.com>
2018-09-07 19:01:31 -0400

    fix link, add github mark to button, and fix layout

--- src/OpenSource.js
+++ src/OpenSource.js
@@ -1,28 +1,28 @@
 import React from 'React'
 import {Box, Heading, Text, Link, FlexContainer} from 'primer-react'
-import Image from './Image'
-import CssImage from './CssImage'
 import ButtonFillDark from './ButtonFillDark'
-import ButtonOutline from './ButtonOutline'
-import Octicon, {FileCode, Ruby, Paintcan} from '@githubprimer/octicons-react'
+import Octicon, {Octoface, MarkGithub} from '@githubprimer/octicons-react'
 
 import LinkDark from './LinkDark'
 
 const OpenSource = props => (
-  <Box bg="blue.2" py={6} mt={6}>
-    <FlexContainer className='container-xl p-responsive mx-auto'>
-      <Box mt={[4,3,0]} width={[1, 1, 6/12]} pr={[0, 3, 3, 6]} >
+  <Box bg="blue.2" py={6} px={3} mt={6}>
+    <Box className='container-xl p-responsive mx-auto'>
+    <Box mx={-4} className="d-flex flex-wrap">
+      <Box mb={[5,5,0]} width={[1, 1, 6/12]} px={4} >
         <Heading lineHeight="1.25" color="black" mb={3} fontSize={7} fontWeight='bold'>Open source</Heading>
         <Text is='p' color="black" mb={4} fontSize={3}>Primer is open sourced on GitHub. Contributions and feedback are welcome!</Text>
-        <ButtonFillDark mr={2} href='https://atom.io/packages/autocomplete-primer'>Fork on GitHub</ButtonFillDark>
+        <ButtonFillDark mr={2} href='https://github.com/primer'>
+        <Octicon color="blue.2" icon={MarkGithub} size='20' verticalAlign="text-bottom" className="mr-2"/>Contribute on GitHub</ButtonFillDark>
       </Box>
-      <Box my={[4,3,0]} width={[1, 1, 6/12]} pr={[0, 3, 3, 6]} >
+      <Box width={[1, 1, 6/12]} px={4} >
         <Heading lineHeight="1.25" color="black" mb={3} fontSize={7} fontWeight='bold'>Keep in touch</Heading>
         <LinkDark fontSize={2} mb={2} className="d-block" href="https://twitter.com/githubprimer">Follow us on twitter</LinkDark>
         <LinkDark fontSize={2} mb={2} className="d-block" href="https://spectrum.chat/primer">Chat to us in Spectrum</LinkDark>
         <LinkDark fontSize={2} mb={2} className="d-block" href="https://github.com/primer/primer/issues/new/choose">Share feedback on GitHub</LinkDark>
       </Box>
-    </FlexContainer>
+      </Box>
+    </Box>
     <Box color="black" className="container-xl p-responsive mx-auto">
       <Box mt={6} pt={4} borderTop={2} borderColor="black">
         <Text is="p">Copyright GitHub 2018. Available for use under the <LinkDark className="d-inline" fontWeight="bold" href="https://github.com/primer/primer/blob/master/LICENSE">MIT license</LinkDark>.</Text>
