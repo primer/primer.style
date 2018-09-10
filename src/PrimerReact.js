1ed96785a7a45b514553c276700465d30b01bf44
Diana Mounter <broccolini@github.com>
2018-09-10 11:50:06 -0400

    fix button & link layout on mobile

--- src/PrimerReact.js
+++ src/PrimerReact.js
@@ -16,9 +16,9 @@ const PrimerReact = props => (
       <Text fontSize={2} ml={1} className="v-align-text-top">BETA</Text>
     </Heading>
     <Text is='p' color="blue.2" mb={4} mx={[-2,0]} fontSize={[2,3]}>We&#8217;re building a React.js component library for Primer. Public beta coming soon!</Text>
-    <FlexContainer width={1, 10/12} mx={'auto'} alignItems="center" className="flex-justify-center">
-        <LinkLight fontSize={2} mr={4} mt={-2} href="https://primer.github.io/primer-react/">View component library</LinkLight>
-        <Link href="https://codesandbox.io/s/1o7v0ryy3q?hidenavigation=1">
+    <FlexContainer width={[1, 10/12]} mx={'auto'} alignItems="center" className="flex-justify-center flex-wrap flex-md-nowrap">
+        <LinkLight fontSize={2} mx={2} mt={-2} mb={[3,0]} href="https://primer.github.io/primer-react/">View component library</LinkLight>
+        <Link mx={2} mb={[3,0]} href="https://codesandbox.io/s/1o7v0ryy3q?hidenavigation=1">
           <img alt="Edit on Code Sandbox" src="https://codesandbox.io/static/img/play-codesandbox.svg"/>
         </Link>
     </FlexContainer>
