e9cebbb5c1c4c315d3bcf328655c818fe304be71
Diana Mounter <broccolini@github.com>
2018-09-10 11:39:45 -0400

    remove links from nav

--- src/Nav.js
+++ src/Nav.js
@@ -5,28 +5,14 @@ import LinkLight from './LinkLight'
 import Octicon, {MarkGithub} from '@githubprimer/octicons-react'
 
 const Nav = props => (
-  <Box bg='gray.9' py={3} className="box-shadow-large">
-    <div className='d-flex flex-items-center flex-wrap flex-sm-nowrap p-responsive'>
+  <Box bg='gray.9' py={3}>
+    <div className='d-flex flex-items-center p-responsive'>
       <LinkLight nounderline pl={3} href='/'>
         <Box color='blue.2' className="d-flex flex-items-center">
           <Octicon color="blue.2" icon={MarkGithub} ariaLabel="GitHub Primer home" size='medium' />
           <Text mx={3} color="blue.2" fontSize="2" lineHeight="condensed" >Primer</Text>
         </Box>
       </LinkLight>
-      <Box mt={[3, 0]} className='flex-auto text-left, text-md-right'>
-        <NavLink
-        href='https://styleguide.github.com/primer/'
-        title='Style guide'
-        />
-        <NavLink
-          href='https://spectrum.chat/primer'
-          title='Community'
-        />
-        <NavLink
-          href='https://github.com/primer/'
-          title='Open-source'
-        />
-      </Box>
     </div>
   </Box>
 )
