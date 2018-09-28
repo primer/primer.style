ccd7e80239f349b30d1c90795aa74d3dc32853a0
Diana Mounter <broccolini@github.com>
2018-09-28 19:18:33 -0400

    remove BoxPro

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import BoxPro from '../src/BoxPro'
+import {Box} from '@primer/components'
 import {ThemeProvider} from 'emotion-theming'
 import theme from '../src/theme.js'
 
 const Page = props => (
   <ThemeProvider theme={theme}>
-    <BoxPro bg='black' color='blue.2'>
+    <Box bg='black' color='blue.2'>
       {props.children}
-    </BoxPro>
+    </Box>
   </ThemeProvider>
 )
 
