db14ea349bfb95d61c02231ae4b5a09f8c79d234
Emily <emplums@github.com>
2018-10-01 15:51:47 -0700

    fix typo

--- src/NavLink.js
+++ src/NavLink.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const NavLink = props => (
-  <Light color="blue.3" fontSize={1} mx={3} href={props.href}>
+  <Link color="blue.3" fontSize={1} mx={3} href={props.href}>
     {props.title}
   </Link>
 )
