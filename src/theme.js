a5ceaab36b1fdab5304629bd24e281524a910546
Emily <emplums@github.com>
2018-10-03 09:26:23 -0700

    remove broken space property

--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,6 @@ import primitives from 'primer-primitives'
 const theme = {
   colors: primitives.colors,
   fontSizes: primitives.fontSizes,
-  space: primitives.space.space
 }
 
 export default theme
