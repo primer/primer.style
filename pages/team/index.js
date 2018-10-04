4df4c4b8ed7b3bc48d995ce47c4a9502db1c5ab6
Jon Rohan <rohan@github.com>
2018-10-04 13:30:15 -0700

    Updating some images

--- pages/team/index.js
+++ pages/team/index.js
@@ -9,8 +9,8 @@ export default () => (
     <Box p={4}>
       <Text fontSize={5}>Meet the team</Text>
 
-      <AvatarShape shape='hexagon' src='https://github.com/broccolini.png'/>
-      <AvatarShape shape='diamond' src='https://github.com/jonrohan.png'/>
+      <AvatarShape shape='hexagon' src='/images/broccolini.jpg'/>
+      <AvatarShape shape='diamond' src='/images/jonrohan.jpg'/>
       <AvatarShape shape='square' src='https://github.com/shawnbot.png'/>
       <AvatarShape shape='circle' src='https://github.com/emplums.png'/>
       <AvatarShape shape='diamond' src='https://github.com/emilybrick.png'/>
