abb003bb26eade1cb0827792dd68b9e0903e023c
Shawn Allen <shawn.allen@github.com>
2018-10-04 12:56:32 -0700

    lint

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,7 @@ import primitives from 'primer-primitives'
 
 const theme = {
   colors: primitives.colors,
-  fontSizes: primitives.fontSizes,
+  fontSizes: primitives.fontSizes
 }
 
 export default theme
