543de9dd77c97398b54b323915b16906055ea5a1
Diana Mounter <broccolini@github.com>
2018-10-04 14:57:57 -0700

    replace boxes with grid component in PrimerCSS

--- src/PrimerCSS.js
+++ src/PrimerCSS.js
@@ -2,32 +2,33 @@ import React from 'react'
 import {Box, Heading, Text, Link, FlexContainer, FlexItem} from '@primer/components'
 import Octicon, {Package} from '@githubprimer/octicons-react'
 import CssImage from './svg/Css.svg'
+import IndexGrid from './IndexGrid'
 import ButtonFill from './ButtonFill'
 import ButtonOutline from './ButtonOutline'
 
 
 const PrimerCSS = props => (
 <Box>
-  <Box my={[0, 3, 5]} mx={'auto'} px={4} className="container-xl">
-    <Box mx={-4} className="d-flex flex-wrap flex-items-center">
-      <Box width={[1, 1, 6/12]} px={4}>
-        <Box p={[3, 3, 5]} mb={[3,3,0]} bg="blue.8" borderRadius={6}>
-          <CssImage />
-        </Box>
+  <IndexGrid my={[0, 3, 5]}>
+    <IndexGrid.Item>
+      <Box p={[3, 3, 5]} mb={[3,3,0]} bg="blue.8" borderRadius={6}>
+        <CssImage />
       </Box>
-      <Box my={[4,3,0]} width={[1, 1, 6/12]} px={4}>
-        <Heading lineHeight="1.25" color="blue.4" mb={3} fontSize={5} fontWeight='bold'>Highly reusable, flexible styles</Heading>
-        <Text is='p' color="blue.2" mb={4} fontSize={3}>Styles can be mixed and matched to achieve many different layouts, independent of their location.</Text>
-        <ButtonFill mr={2} href='https://styleguide.github.com/primer/'>Documentation</ButtonFill>
-        <ButtonOutline href='https://github.com/primer/primer'>GitHub</ButtonOutline>
-        <Text is="p" fontSize={2} mt={4} color="blue.3" className="text-mono">npm i primer@latest</Text>
-      </Box>
-    </Box>
-  </Box>
-  <Box mt={2} px={4} className="container-xl">
-    <Box mb={4} pb={2}>
+    </IndexGrid.Item>
+    <IndexGrid.Item my={[4,3,0]}>
+      <Heading lineHeight="1.25" color="blue.4" mb={3} fontSize={5} fontWeight='bold'>Highly reusable, flexible styles</Heading>
+      <Text is='p' color="blue.2" mb={4} fontSize={3}>Styles can be mixed and matched to achieve many different layouts, independent of their location.</Text>
+      <ButtonFill mr={2} href='https://styleguide.github.com/primer/'>Documentation</ButtonFill>
+      <ButtonOutline href='https://github.com/primer/primer'>GitHub</ButtonOutline>
+      <Text is="p" fontSize={2} mt={4} color="blue.3" className="text-mono">npm i primer@latest</Text>
+    </IndexGrid.Item>
+  </IndexGrid>
+  <IndexGrid>
+    <IndexGrid.Item mt={2} mb={4} pb={2}>
       <Text fontSize={5} color="blue.2" lineHeight={1.25}>Install only the packages you need.</Text>
-    </Box>
+    </IndexGrid.Item>
+  </IndexGrid>
+  <Box px={4} className="container-xl">
     <Box mx={-4} className="d-flex flex-wrap flex-items-start">
       <FlexContainer width={[1, 6/12, 6/12]} px={4} mb={[3, 4, 4, 0]}>
         <FlexItem color="blue.3" mr={3}>
