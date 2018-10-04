3ff03e76575c450a393a263d2aa88e9bb6871006
Emily <emplums@github.com>
2018-10-04 13:43:50 -0700

    fix BackgroundImage component

--- src/BackgroundImage.js
+++ src/BackgroundImage.js
@@ -5,18 +5,7 @@ import {Box} from '@primer/components'
 import styled from 'react-emotion'
 
 const BackgroundImage = styled(Box)`
-  background-color: 'transparent'
-  &:after {
-    background: url('./line.svg');
-    background-size: 662px 300px;
-    background-repeat: no-repeat;
-    content: "";
-    height: 100%;
-    width: 100%;
-    position: absolute;
-    top: 200px;
-    left: 65px;
-  }
+  ${backgroundImage}
 `
 
 export default BackgroundImage
