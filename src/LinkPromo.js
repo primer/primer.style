a5929444eef548ccfbf4b64e4a48abef5537a884
Shawn Allen <shawn.allen@github.com>
2018-10-04 16:19:43 -0700

    lint LinkPromo, use themeGet

--- src/LinkPromo.js
+++ src/LinkPromo.js
@@ -1,11 +1,10 @@
 // Extends Link from primer/components to make color primitives available. Ideally I'd use defaultProps here but because we use !important on utilities the theme colors won't override. We could probably add a function to handle this.
-
 import {Link} from '@primer/components'
-import {color, fontSize} from 'styled-system'
+import {themeGet} from 'styled-system'
 import styled from 'react-emotion'
 
 const LinkPromo = styled(Link)`
-  color: ${props => props.theme.colors.orange[3]} !important;
+  color: ${themeGet('colors.orange.3')} !important;
 `
 
 export default LinkPromo
