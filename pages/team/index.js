5328e915f52da849dc2a994abd0cd7b553c3bc5e
Shawn Allen <shawn.allen@github.com>
2018-10-04 13:42:43 -0700

    name default exports in news and team pages

--- pages/team/index.js
+++ pages/team/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import {Box, Text} from '@primer/components'
 import Nav from '../../src/Nav'
 
-export default () => (
-  <Box>
-    <Nav />
-    <Box p={4}>
-      <Text fontSize={5}>Meet the team</Text>
+export default function Team() {
+  return (
+    <Box>
+      <Nav />
+      <Box p={4}>
+        <Text fontSize={5}>Meet the team</Text>
+      </Box>
     </Box>
-  </Box>
-)
+  )
+}
