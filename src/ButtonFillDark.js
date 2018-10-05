763e7ed17f1ed868913dad40cc375a89b99fbdc6
Shawn Allen <shawn.allen@github.com>
2018-10-04 21:00:23 -0700

    use themeGet() in ButtonFillDark

--- src/ButtonFillDark.js
+++ src/ButtonFillDark.js
@@ -1,12 +1,12 @@
 // Extends Link from primer/components to make color primitives available, as well as fontSize and space which should be added in primer/components. Ideally I'd use defaultProps here but because we use !important on utilities the theme colors won't override. We could probably add a function to handle this.
 
 import {Link} from '@primer/components'
-import {color, fontSize, space} from 'styled-system'
+import {space, themeGet} from 'styled-system'
 import styled from 'react-emotion'
 
 const ButtonOutlineDark = styled(Link)`
-  color: ${props => props.theme.colors.blue[2]} !important;
-  background-color: ${props => props.theme.colors.black};
+  color: ${themeGet('colors.blue.2')} !important;
+  background-color: ${themeGet('colors.black')};
   padding: 0.75em 1.25em;
   font-size: ${props => props.theme.fontSizes[2]};
   border-radius: 6px;
@@ -14,8 +14,8 @@ const ButtonOutlineDark = styled(Link)`
   font-weight: 600;
   display: inline-block;
   &:hover {
-    color: ${props => props.theme.colors.white} !important;
-    background-color: ${props => props.theme.colors.gray[9]};
+    color: ${themeGet('colors.white')} !important;
+    background-color: ${themeGet('colors.gray.9')};
     text-decoration: none;
   }
   ${space};
