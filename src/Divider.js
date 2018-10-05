448f713aae47b10cbd0b29b199f69741656c1ea6
Shawn Allen <shawn.allen@github.com>
2018-10-04 21:00:54 -0700

    use themeGet() in Divider

--- src/Divider.js
+++ src/Divider.js
@@ -1,9 +1,9 @@
 import {Box} from '@primer/components'
-import {color} from 'styled-system'
+import {themeGet} from 'styled-system'
 import styled from 'react-emotion'
 
 const Divider = styled(Box)`
-  border-top: 2px solid ${props => props.theme.colors.blue[3]};
+  border-top: 2px solid ${themeGet('colors.blue.3')};
   opacity: 0.15;
   width: 10%;
   margin-right: auto;
