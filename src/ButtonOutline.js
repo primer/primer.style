3b2089c589c1b68ebbbe8578b32ec27aaa01880e
Shawn Allen <shawn.allen@github.com>
2018-10-04 21:00:40 -0700

    lint ButtonOutline, use themeGet()

--- src/ButtonOutline.js
+++ src/ButtonOutline.js
@@ -1,12 +1,12 @@
 // Extends Link from primer/components to make color primitives available, as well as fontSize and space which should be added in primer/components. Ideally I'd use defaultProps here but because we use !important on utilities the theme colors won't override. We could probably add a function to handle this.
 
 import {Link} from '@primer/components'
-import {color, fontSize, space} from 'styled-system'
+import {space, themeGet} from 'styled-system'
 import styled from 'react-emotion'
 
 const ButtonOutline = styled(Link)`
-  color: ${props => props.theme.colors.blue[3]} !important;
-  border-color: ${props => props.theme.colors.blue[3]};
+  color: ${themeGet('colors.blue.3')} !important;
+  border-color: ${themeGet('colors.blue.3')};
   border-width: 1px;
   border-style: solid;
   padding: 0.75em 1.25em;
@@ -15,11 +15,12 @@ const ButtonOutline = styled(Link)`
   font-size: 16px;
   font-weight: 600;
   display: inline-block;
-  ${space} &:hover {
-    color: ${props => props.theme.colors.blue[4]} !important;
-    border-color: ${props => props.theme.colors.blue[4]};
+  &:hover {
+    color: ${themeGet('colors.blue.4')} !important;
+    border-color: ${themeGet('colors.blue.4')};
     text-decoration: none;
   }
+  ${space};
 `
 
 export default ButtonOutline
