fa1f91fda3406271f10e34eb31d9dc136b1e465d
Shawn Allen <shawn.allen@github.com>
2018-10-04 20:58:35 -0700

    use themeGet() in LinkDark

--- src/LinkDark.js
+++ src/LinkDark.js
@@ -1,13 +1,13 @@
 // Extends Link from primer/components to make color primitives available. Ideally I'd use defaultProps here but because we use !important on utilities the theme colors won't override. We could probably add a function to handle this.
 
 import {Link} from '@primer/components'
-import {color, fontSize, fontWeight} from 'styled-system'
+import {themeGet} from 'styled-system'
 import styled from 'react-emotion'
 
 const LinkDark = styled(Link)`
-  color: ${props => props.theme.colors.black} !important;
+  color: ${themeGet('colors.black')} !important;
   &:hover {
-    color: ${props => props.theme.colors.gray[8]};
+    color: ${themeGet('colors.gray.8')};
   }
 `
 
