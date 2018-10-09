6945b5137fca71faec3b259847660296719a3d03
Emily <emplums@github.com>
2018-10-09 11:18:33 -0700

    resposnive changes

--- pages/team/index.js
+++ pages/team/index.js
@@ -21,7 +21,7 @@ const TeamIndex = () => (
   <Box>
     <Nav />
     <Box className="container-xl" pt={7} px={5} style={{overflow: 'hidden'}}>
-      <FlexContainer justifyContent="space-between" flexDirection={['column', 'column', 'row', 'row']} mb={10} mx={-5}>
+      <FlexContainer justifyContent="space-between" flexDirection={['column-reverse', 'column-reverse', 'column-reverse', 'row', 'row']} mb={10} mx={-5}>
         <Box width={[1, 1, 1, 6/12]} pt={8} pb={12} px={5}>
           <Heading fontSize={7} color="blue.4" lineHeight={1} mb={3}>
             Meet the team
--- src/MemberContainer.js
+++ src/MemberContainer.js
@@ -3,22 +3,14 @@ import {FlexContainer, Box, FlexItem} from '@primer/components'
 import MemberQuestions from './MemberQuestions'
 import AvatarShape from './AvatarShape'
 
-const oddLineStyles = `
-  margin-right: 128px;
-  }`
-
-const evenLineStyles = `
-  margin-left: 128px;
-  }`
-
 const direction = isOdd =>
-  isOdd ? ['column-reverse', 'column-reverse', 'row-reverse', 'row-reverse'] : ['column-reverse', 'column-reverse', 'row', 'row']
+  isOdd ? ['column-reverse', 'column-reverse', 'column-reverse', 'row-reverse', 'row-reverse'] : ['column-reverse', 'column-reverse', 'column-reverse', 'row', 'row']
 
 const Member = ({member, isOdd, shape}) => (
-  <FlexContainer mb={12} justifyContent="flex-end" flexDirection={direction(isOdd)}>
+  <FlexContainer mb={12} justifyContent="flex-end" alignItems={['center', 'center', 'center', 'initial', 'initial']} flexDirection={direction(isOdd)}>
     <MemberQuestions member={member} />
-    <FlexItem flexShrink="0" style={{position: 'relative'}}>
-      <Box css={isOdd ? oddLineStyles : evenLineStyles}>
+    <FlexItem mb={[12, 12, 12, 0, 0]} flexShrink="0" style={{position: 'relative'}}>
+      <Box mr={isOdd ? [0, 0, 0, 12, 12] : 0} ml={isOdd ? 0 : [0, 0, 0, 12, 12]}>
         <AvatarShape shape={shape} src={member.avatar} />
       </Box>
     </FlexItem>
