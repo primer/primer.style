6b9353db07feee90c70d8b337af887712632baa6
Diana Mounter <broccolini@github.com>
2018-10-09 10:46:02 -0400

    team header typography & spacing

--- pages/team/index.js
+++ pages/team/index.js
@@ -20,14 +20,15 @@ const getMemberContent = () => {
 const TeamIndex = () => (
   <Box>
     <Nav />
-          <Heading fontSize={7} color="blue.4">
     <Box className="container-xl" pt={7} px={5} style={{overflow: 'hidden'}}>
       <FlexContainer justifyContent="space-between" flexDirection={['column', 'column', 'row', 'row']} mb={10} mx={-5}>
         <Box width={[1, 1, 1, 6/12]} pt={8} pb={12} px={5}>
+          <Heading fontSize={7} color="blue.4" lineHeight={1} mb={3}>
             Meet the team
           </Heading>
-          <Text fontSize={4} color="blue.2">
-            Get to know the team behind Primer; how we got into design systems and what we’re most looking forward to.
+          <Text fontSize={4} color="blue.2" lineHeight={1.25}>
+            Get to know the team behind Primer; how we got into design systems and what we’re most looking forward to
+            Meet the GitHub Design Systems team
           </Text>
         </Box>
         <Relative top="-100px">
