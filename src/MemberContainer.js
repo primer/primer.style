a4a6f50b7fb74976221dd8785d2864458f0fedcf
Emily <emplums@github.com>
2018-10-09 19:56:14 -0700

    dot svg changes

--- src/MemberContainer.js
+++ src/MemberContainer.js
@@ -2,32 +2,25 @@ import React from 'react'
 import {FlexContainer, Box, FlexItem} from '@primer/components'
 import MemberQuestions from './MemberQuestions'
 import AvatarShape from './AvatarShape'
+import DotsSVG from  './svg/dots.svg'
 
 const oddDots = `
-  &:after {
-    background-image: url(./dots.svg);
-    background-size: 168px 92px;
-    background-repeat: no-repeat;
-    content: "";
-    height: 92px;
-    width: 168px;
-    position: absolute;
-    top: 220px;
-    left: 145px;
-    transform: rotate(-180deg);
-  }`
+{
+  height: 92px;
+  width: 168px;
+  position: absolute;
+  top: 220px;
+  left: 145px;
+  transform: rotate(-180deg);
+}`
 const evenDots = `
-  &:after {
-    background-image: url(./dots.svg);
-    background-size: 168px 92px;
-    background-repeat: no-repeat;
-    content: "";
-    height: 92px;
-    width: 168px;
-    position: absolute;
-    top: 234px;
-    left: 46px;
-  }`
+{
+  height: 92px;
+  width: 168px;
+  position: absolute;
+  top: 234px;
+  left: 46px;
+}`
 
 const direction = isOdd =>
   isOdd
@@ -43,8 +36,9 @@ const Member = ({member, isOdd, shape}) => (
   >
     <MemberQuestions member={member} />
     <FlexItem mb={[6, 8, 8, 0, 0]} flexShrink="0" style={{position: 'relative'}}>
-      <Box css={isOdd ? oddDots : evenDots} mr={isOdd ? [0, 0, 0, 12, 12] : 0} ml={isOdd ? 0 : [0, 0, 12, 12, 12]}>
+      <Box mr={isOdd ? [0, 0, 0, 12, 12] : 0} ml={isOdd ? 0 : [0, 0, 12, 12, 12]}>
         <AvatarShape shape={shape} src={member.avatar} />
+        <DotsSVG css={isOdd ? oddDots : evenDots} />
       </Box>
     </FlexItem>
   </FlexContainer>
