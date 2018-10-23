5afa897e121ffb57119745ec3f66d7bd377723d3
Joshua Shao <jhuashao@github.com>
2018-10-23 15:43:32 -0700

    functional orange edits

--- src/MemberQuestions.js
+++ src/MemberQuestions.js
@@ -11,38 +11,38 @@ injectGlobal`
   }
 `
 
-const MemberInfo = ({member, color}) => {
+const MemberInfo = ({member, colorName}) => {
   return (
     <Box width={[1, 1, 1, 1 / 2]} id={member.handle}>
-      <Heading fontSize={5} pb={3} lineHeight={1.25} color={`${color}.4`}>
+      <Heading fontSize={5} pb={3} lineHeight={1.25} color={`${colorName}.4`}>
         {member.name}, {member.title}
       </Heading>
       <Link
         mt={2}
         fontFamily="mono"
-        color={`${color}.3`}
-        hoverColor={`${color}.3`}
+        color={`${colorName}.3`}
+        hoverColor={`${colorName}.3`}
         fontSize={2}
         href={`https://github.com/${member.handle}`}
       >
-        <Octicon icon={MarkGithub} size={24} verticalAlign="middle" color={`${color}.3`} mr={3} />@{member.handle}
+        <Octicon icon={MarkGithub} size={24} verticalAlign="middle" color={`${colorName}.3`} mr={3} />@{member.handle}
       </Link>
-      <Text fontFamily="mono" color={`${color}.4`} is="div" fontSize={3} mt={7} mb={0}>
+      <Text fontFamily="mono" color={`${colorName}.4`} is="div" fontSize={3} mt={7} mb={0}>
         What drew you into design systems?
       </Text>
-      <Text color={`${color}.1`} fontSize={3}>
+      <Text color={`${colorName}.1`} fontSize={3}>
         <ReactMarkdown className="markdown" source={member.questionOne} />
       </Text>
-      <Text fontFamily="mono" color={`${color}.4`} is="div" fontSize={3} mt={7} mb={0}>
+      <Text fontFamily="mono" color={`${colorName}.4`} is="div" fontSize={3} mt={7} mb={0}>
         Who have you learned from or been inspired by?
       </Text>
-      <Text color={`${color}.1`} fontSize={3}>
+      <Text color={`${colorName}.1`} fontSize={3}>
         <ReactMarkdown className="markdown" source={member.questionTwo} />
       </Text>
-      <Text fontFamily="mono" color={`${color}.4`} is="div" fontSize={3} mt={7} mb={0}>
+      <Text fontFamily="mono" color={`${colorName}.4`} is="div" fontSize={3} mt={7} mb={0}>
         Favorite tools
       </Text>
-      <Text color={`${color}.1`} fontSize={3}>
+      <Text color={`${colorName}.1`} fontSize={3}>
         <ReactMarkdown className="markdown" source={member.favoriteTools} />
       </Text>
     </Box>
