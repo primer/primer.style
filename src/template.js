858f8c7dd8266729fe71b5c3d7696cfa4c7c25ae
Emily Brick <emilybrick@github.com>
2018-10-29 13:43:29 -0400

    fixed typo

--- src/template.js
+++ src/template.js
@@ -17,13 +17,13 @@ module.exports = ({html = '', css = '', scripts, title}) =>
   <link rel="apple-touch-icon" href="https://user-images.githubusercontent.com/334891/45369221-63075b00-b5b3-11e8-8ac7-7e588fe4c905.png">
   <link rel="icon" href="https://user-images.githubusercontent.com/334891/45369187-51be4e80-b5b3-11e8-8066-b06025239a79.png">
   <title>${title}</title>
-  <meta property="og:keywords" content="Design System" />
+  <meta name="keywords" content="Design System" />
   <meta property="og:article:author" content="GitHub Design Systems team" />
   <meta property="og:title" content="Primer" />
   <meta property="og:type" content="website" />
   <meta property="og:url" content="https://primer.style" />
   <meta property="og:description" content="The design system that powers GitHub" />
-  <meta name="og:image" content="https://user-images.githubusercontent.com/586552/47662657-bb011d80-db71-11e8-967e-6d7af28d25d4.png" />
+  <meta property="og:image" content="https://user-images.githubusercontent.com/586552/47662657-bb011d80-db71-11e8-967e-6d7af28d25d4.png" />
   <meta property="og:image:width" content="1200" />
   <meta property="og:image:height" content="630" />
   <meta property="twitter:card" content="summary_large_image" />
