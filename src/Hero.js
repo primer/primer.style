bd21be70c5625b3e6b27d1fa18624710f4b6a006
Joshua Shao <jhuashao@github.com>
2018-11-02 11:10:26 -0700

    initial commit

--- src/Hero.js
+++ src/Hero.js
@@ -1,42 +1,28 @@
 import React from 'react'
 import {Box, Heading, Text, Flex, Absolute, Relative} from '@primer/components'
-import HeroImage from './svg/Hero.svg'
-import HeroOverlay from './svg/HeroOverlay.svg'
-import LinkLight from './LinkLight'
+
+import ReactBodymovin from 'react-bodymovin'
+import TestAnimation from './TestAnimation.json'
+
 import IndexGrid from './IndexGrid'
+
+const bodymovinOptions = {
+  loop: true,
+  autoplay: true,
+  prerender: true,
+  animationData: TestAnimation
+}
 
 export default function Hero() {
   return (
-    <Box pt={[4, 2, 5]}>
-      <IndexGrid my={[6, 6, 12]} flexDirection={['row', 'row', 'row-reverse']} alignItems="end">
-        <Flex.Item px={5} width={[10 / 12, 8 / 12, 8 / 12, 5 / 12]} mx={'auto'}>
-          <Relative mt={[2, 3, 5]}>
-            <HeroImage width="100%" height={null} />
-            <Absolute top={0} left={0} width="100%" height="100%" zIndex={100} css={{pointerEvents: 'none'}}>
-              <HeroOverlay width="100%" height={null} />
-            </Absolute>
+    <Box pt={[4, 2, 0]}>
+      <IndexGrid my={[6, 6, 6]} flexDirection={['row', 'row', 'row-reverse']} alignItems="end">
+        <Flex.Item px={5} width={[10 / 12, 8 / 12, 8 / 12, 12 / 12]} mx={'auto'}>
+          <Relative mt={[2, 3, 0]}>
+            <div>
+              <ReactBodymovin options={bodymovinOptions} />
+            </div>
           </Relative>
-        </Flex.Item>
-        <Flex.Item px={5} my={[4, 3, 0]} width={[1, 1, 1, 7 / 12]}>
-          <Heading color="blue.4" mb={2} fontSize={[48, 56, 84]} fontWeight="bold">
-            Primer
-          </Heading>
-          <Text color="blue.2" fontSize={[4, 5, 5, 7]} lineHeight={1.25}>
-            Resources, tooling, and design guidelines for building interfaces with GitHub’s design system
-          </Text>
-          <Text fontFamily="mono" is="p" color="blue.3" mt={5}>
-            <LinkLight fontSize={[0, 1, 2]} href="https://styleguide.github.com/primer/">
-              Style guide
-            </LinkLight>{' '}
-            ・
-            <LinkLight ml={2} fontSize={[0, 1, 2]} href="https://spectrum.chat/primer">
-              Community
-            </LinkLight>{' '}
-            ・
-            <LinkLight ml={2} fontSize={[0, 1, 2]} href="https://github.com/primer/">
-              Open-source
-            </LinkLight>
-          </Text>
         </Flex.Item>
       </IndexGrid>
     </Box>
