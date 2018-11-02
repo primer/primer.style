8f43dc85bd3c8164b3a7aac3de0ac12e55e8b555
Emily Brick <emilybrick@github.com>
2018-11-02 10:47:45 -0400

    Updated primer.style og image (#76)
    * updated og image and tags
    
    * fixed typo
    
    * added space
    
    * removed space

--- src/template.js
+++ src/template.js
@@ -16,17 +16,18 @@ module.exports = ({html = '', css = '', scripts, title}) =>
   <link rel='stylesheet' href='https://unpkg.com/primer-layout/build/build.css' />
   <link rel="apple-touch-icon" href="https://user-images.githubusercontent.com/334891/45369221-63075b00-b5b3-11e8-8ac7-7e588fe4c905.png">
   <link rel="icon" href="https://user-images.githubusercontent.com/334891/45369187-51be4e80-b5b3-11e8-8066-b06025239a79.png">
-
   <title>${title}</title>
-  <meta name="description" content="The design system that powers GitHub">
-  <meta name="keywords" content="Design System">
-  <meta name="author" content="GitHub Design Systems team">
-
-  <meta name="og:title" content="Primer">
-  <meta name="og:description" content="The design system that powers GitHub">
-  <meta name="og:image" content="https://user-images.githubusercontent.com/334891/45336457-b47efe00-b551-11e8-9644-398f29eceb92.png">
-  <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:site" content="@githubprimer">
+  <meta name="keywords" content="Design System" />
+  <meta property="og:article:author" content="GitHub Design Systems team" />
+  <meta property="og:title" content="Primer" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://primer.style" />
+  <meta property="og:description" content="The design system that powers GitHub" />
+  <meta property="og:image" content="https://user-images.githubusercontent.com/586552/47662657-bb011d80-db71-11e8-967e-6d7af28d25d4.png" />
+  <meta property="og:image:width" content="1200" />
+  <meta property="og:image:height" content="630" />
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:site" content="@githubprimer" />
   ${css}
 </head>
 <body>
