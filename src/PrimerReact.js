8b817e1bdc9483411aaab5e5657f9b988c06427c
Diana Mounter <broccolini@github.com>
2018-12-28 16:00:18 -0500

    remove whitespace

--- src/PrimerReact.js
+++ src/PrimerReact.js
@@ -13,7 +13,7 @@ export default function PrimerReact() {
           BETA
         </Text>
       </Heading>
-      <Text is="p" color="blue.2" mb={3} mx={[-2, 0]} fontSize={[2, 3]}>
+      <Text is="p" color="blue.2" mb={5} mx={[-2, 0]} fontSize={[2, 3]}>
         We&#8217;re building a React.js component library for Primer.
       </Text>
       <Flex flexWrap="wrap" alignItems="center" justifyContent="center">
@@ -24,7 +24,6 @@ export default function PrimerReact() {
           View the component library →
         </LinkLight>
       </Flex>
-
     </Box>
   )
 }
