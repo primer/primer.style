0f23bbe906da183a253a051d0ddd1b948a810ac0
Ash Guillaume <ashygee@github.com>
2019-02-05 14:27:08 -0800

    Added essence gifs to team

--- src/team-content.js
+++ src/team-content.js
@@ -12,7 +12,8 @@ const teamContent = [
 
   The field of Graphic Design has a tremendous influence on me, and some of the designers and studios I’m inspired by are: [Natasha Jen](https://www.pentagram.com/about/natasha-jen), [Barron Webster](https://barronwebster.com/), [Maxime Buschi](https://www.youtube.com/watch?v=diBifN7qBgw), [Michael Bierut](https://www.pentagram.com/about/michael-bierut), [Eric Hu](http://erichu.info/), [Micah Barrett](http://micahbarrett.design/), [Draw Down Books](https://draw-down.com/), [Wei Huang](https://twitter.com/w__h_?lang=en), [Josh Schaub](http://joshschaub.ch/), [Morcoskey](https://morcoskey.com/), and [David Rudnik](https://davidrudnick.org/). I’ve always been inspired by those who are experimenting at the intersection of design and technology: [Tristan Harris](http://www.tristanharris.com/), [Brad Frost](http://bradfrost.com/), [Studio No-Plans](https://no-plans.com/), [Marie Otsuka](http://motsuka.com/), [Ben Wilkins](https://twitter.com/thatbenlifetho?lang=en), [Jon Gold](https://jon.gold/), [Bret Victor](http://worrydream.com/),  and [Chris Novello](http://chrisnovello.com/). I also love typography, and have a lot of admiration for type-designers and foundries: [Colophon Foundry](https://www.colophon-foundry.org/), [Kris Sowersby (Klim Type)](https://klim.co.nz/), [Matthew Carter](https://www.myfonts.com/person/Matthew_Carter/), [Richard Lipton](https://lipton.typenetwork.com/), [Lucas Sharp](http://lucassharp.com/), [Grilli Type](https://www.grillitype.com/), and [James Edmondson (OHnotypeco)](https://ohnotype.co/). There’s so many more people that I’ve learned and drawn inspiration from to mention and praise, so feel free to reach out! I’d love to share and discuss. <3`,
     favoriteTools: 'VScode, Figma, Adobe Creative Suite, Zero-Sugar Redbull',
-    avatar: 'https://user-images.githubusercontent.com/24916540/47465791-490c8b00-d7a3-11e8-8dc8-9cc968a47b62.jpg'
+    avatar: 'https://user-images.githubusercontent.com/24916540/47465791-490c8b00-d7a3-11e8-8dc8-9cc968a47b62.jpg',
+    gif: 'https://user-images.githubusercontent.com/24916540/50362777-e2df8280-051d-11e9-8225-a0ed066eac59.gif'
   },
   {
     name: 'Ash Guillaume',
@@ -24,7 +25,8 @@ const teamContent = [
 
   When I first started my career, [Frank Chimero](https://frankchimero.com/) was a huge inspiration. His book, [The Shape of Design](https://frankchimero.com/design/the-shape-of-design/), really helped to form my philosophy and approach to problems and how to cultivate a design process. Along with that but not design related, [Jason Fried](https://twitter.com/jasonfried) has been a great inspiration to how I approach work. His book (written alongside David Heinemeier Hansson) [Rework](https://basecamp.com/books/rework) still is the best, no nonsense, business book I've ever read.`,
     favoriteTools: 'Figma, Atom, Keynote, Bear Notes, Spark Mail, Spotify',
-    avatar: 'https://user-images.githubusercontent.com/10384315/48450021-61941380-e759-11e8-90e0-7b41f6cbb6e8.jpg'
+    avatar: 'https://user-images.githubusercontent.com/10384315/48450021-61941380-e759-11e8-90e0-7b41f6cbb6e8.jpg',
+    gif: 'https://camo.githubusercontent.com/c325b2b42ec71db51a92d7cdb2a6684824bc0464/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f7854687557686f614e794e426a54474552612f67697068792e676966'
   },
   {
     name: 'Emily Brick',
@@ -34,7 +36,8 @@ const teamContent = [
     questionOne: `I was initially drawn to design systems while working on a small team, where we were stretched thin and struggling to keep the front-end and user experience afloat, recreating components and patterns by hand over and over again. We needed consistency, stability, a bridge between designers and developers, and a more efficient workflow. Once we put a design language and system in place, I was sold on the value. I find it fascinating how many approaches to design systems there are out there, and how different they can be depending on the individual product and teams’ needs.`,
     questionTwo: `[Inge Druckrey](https://www.youtube.com/watch?v=ldSkPqZKBl0) and [Edward Tufte](https://www.edwardtufte.com/tufte/books_ei) inspired me to get into design, and introduced me to the world of typographic and informational systems (simultaneously). This talk by [Bret Victor](https://vimeo.com/36579366) helped me feel less alone when I was starting out in the industry; at a time when code and design weren't yet openly intertwined. I’m also inspired by the folks behind [designbetter.co](https://www.designbetter.co/design-systems-handbook) for making knowledge around design systems transparent and available to everyone.  Additionally, and there are too many people to name, people like [Una Kravetz](https://twitter.com/Una?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor), [Sarah Drasner](https://twitter.com/sarah_edo), [Tim Holman](https://twitter.com/twholman), and [Val Head](https://twitter.com/vlh) are always on my radar, keeping the internet weird and creative and continuously pushing the boundaries of what's possible.`,
     favoriteTools: 'Atom, Figma, Bear, Dash, Slack',
-    avatar: 'https://user-images.githubusercontent.com/586552/46690197-b4fdc980-cbcf-11e8-9ad2-57894e3dee92.jpg'
+    avatar: 'https://user-images.githubusercontent.com/586552/46690197-b4fdc980-cbcf-11e8-9ad2-57894e3dee92.jpg',
+    gif: 'https://user-images.githubusercontent.com/586552/50305051-ba914e80-045f-11e9-9a13-b0e918da2086.gif'
   },
   {
     name: 'Emily Plummer',
@@ -46,7 +49,8 @@ const teamContent = [
      , Mina Markham's [slay.sh](https://slay.sh/), Una Kravets, Suz Hinton's
      [talk about accessibility at Dinosaur.js](https://www.youtube.com/watch?v=hKIQkgPVXH4), Marcy Sutton, and Anil Dash.`,
     favoriteTools: 'Figma, Notion (for taking notes & planning), React',
-    avatar: 'https://user-images.githubusercontent.com/586552/46690198-b4fdc980-cbcf-11e8-95e3-c3e28b69a29c.jpg'
+    avatar: 'https://user-images.githubusercontent.com/586552/46690198-b4fdc980-cbcf-11e8-95e3-c3e28b69a29c.jpg',
+    gif: 'https://camo.githubusercontent.com/4646a6d24f7c08a84028f83987a8ec8462179c11/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f6441775547537553356b7157732f67697068792e676966'
   },
   {
     name: 'Shawn Allen',
@@ -56,7 +60,8 @@ const teamContent = [
     questionOne: `I worked in data visualization and map-making for most of my career, and solving design problems with data is my jam. To me there's something uniquely exciting about making a thing that can take complicated and/or unexpected input and, with no human intervention, reduce it to something visually comprehensible—and hopefully interesting!`,
     questionTwo: `Anyone who has ever contributed to [A List Apart](http://alistapart.com/) or posted to the [webdesign-l](http://www.webdesign-l.com/) mailing list between 1999 and 2005. The [CSS Zen Garden](http://www.csszengarden.com/) was hugely inspirational for me, along with web standards pioneers like Eric Meyer, Mark Pilgrim, and Jeff Zeldman. [Jina](https://twitter.com/jina) is one of the hardest-working people in design systems, and her leadership in the community is a tremendous inspiration. I learned everything I know about SVG from [Sara Soueidan](https://twitter.com/SaraSoueidan), and everything I could _hope_ to know about CSS Grid from [Rachel Andrew](https://twitter.com/rachelandrew) and [Jen Simmons](https://twitter.com/jensimmons). If you want to learn about web components, follow [Monica Dinculescu](https://twitter.com/notwaldorf) and [Mu-An Chiou](https://twitter.com/muanchiou).`,
     favoriteTools: 'Vim, Things, Chrome DevTools, curl',
-    avatar: 'https://user-images.githubusercontent.com/586552/46751266-f81b7380-cc87-11e8-8459-2166a1c40f63.jpg'
+    avatar: 'https://user-images.githubusercontent.com/586552/46751266-f81b7380-cc87-11e8-8459-2166a1c40f63.jpg',
+    gif: 'https://camo.githubusercontent.com/f96029b65ae0f8a1592b30f5449b7115f6560d98/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f5374695957574e7857635741302f67697068792e676966'
   },
   {
     name: 'Jon Rohan',
@@ -66,7 +71,8 @@ const teamContent = [
     questionOne: `I have had a fascination with front end engineering and UI for all of my career. Early on at GitHub I recognized the need to organize our code into reusable components. When the team formalized I had to be involved.`,
     questionTwo: `Both past and present I have to acknowledge that Nicole Sullivan has been profoundly influential in the way I front-end and write CSS. When she introduced concepts of OOCSS it brought me from thinking very linearly about CSS to a whole new range of systems thinking.`,
     favoriteTools: `Atom, Bash/Shell, Figma, GitHub, iTerm, Javascript, MacBook Pro, Things`,
-    avatar: 'https://user-images.githubusercontent.com/586552/46690199-b4fdc980-cbcf-11e8-9f8d-59a694281b70.jpg'
+    avatar: 'https://user-images.githubusercontent.com/586552/46690199-b4fdc980-cbcf-11e8-9f8d-59a694281b70.jpg',
+    gif: 'https://user-images.githubusercontent.com/54012/50536666-fced1680-0b56-11e9-89e6-995cdb01a599.gif'
   },
   {
     name: 'Diana Mounter',
@@ -77,7 +83,8 @@ const teamContent = [
       'I was drawn to design systems for many reasons—the opportunity to improve design and development workflow efficiency; the potential benefits well-design systems can bring to the user experience; I like to bring order to chaos and enjoy working at the intersection of design and code. I also love that there is still so much to learn in the field and really enjoy the passion of the community behind design systems.',
     questionTwo: `I don't have room on this webpage to list everyone! [SMACSS](https://smacss.com/), [The Art and Science of CSS](https://www.amazon.com/Science-Jonathan-Bolton-Cameron-Paperback/dp/B00ZVORUW6), and [Veerle Pieters](http://veerle.duoh.com/) were some early influences. [OOCSS](https://github.com/stubbornella/oocss/wiki) by Nicole Sullivan, [CSS Wizardary](https://csswizardry.com/), [Basscss](http://basscss.com/), [Tachyons](https://tachyons.io/), and [BEM](http://getbem.com/naming/) influenced my approach to CSS architecture. More recently I've been inspired by Rune Madsen's [Programming Design Systems](https://programmingdesignsystems.com), [Compositor](https://github.com/c8r), [Dan Abramov](https://twitter.com/dan_abramov), [Sebastian Markbage](https://youtu.be/4anAwXYqLG8), and so many design and code wonder women like [Many Kerr](https://twitter.com/mandy_kerr), [Marcy Sutton](https://twitter.com/marcysutton), [Lea Verou](https://twitter.com/LeaVerou), [Mina Markham](https://twitter.com/MinaMarkham), [Sarah Drasner](https://twitter.com/sarah_edo) and [Jina Anne](https://twitter.com/jina).`,
     favoriteTools: 'Hyper, Atom, Figma, MDN, and Quip',
-    avatar: 'https://user-images.githubusercontent.com/586552/46751001-5c8a0300-cc87-11e8-84bb-9024b03df74f.jpg'
+    avatar: 'https://user-images.githubusercontent.com/586552/46751001-5c8a0300-cc87-11e8-84bb-9024b03df74f.jpg',
+    gif: 'https://user-images.githubusercontent.com/334891/50363219-bab14d00-0538-11e9-9595-48b3c45358ab.gif'
   }
 ]
 
