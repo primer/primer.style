0d228010e11b81c6fd08383b0f91daffb6641f3e
Ash Guillaume <ashygee@github.com>
2019-03-08 12:14:33 -0800

    added extra favicon sizes

--- src/icons/index.js
+++ src/icons/index.js
@@ -19,11 +19,15 @@ const link = ({rel = 'icon', type = 'image/png', file}) => `<link rel="${rel}" t
 const meta = ({name, file}) => `<link name="${name}" content="${path(file)}">`
 
 module.exports = [
-  link({rel: 'icon', type: 'image/png', file: 'favicon.png'}),
-  link({rel: 'apple-touch-icon', file: 'touch-icon-iphone.png'}),
+  link({rel: 'icon', type: 'image/png', sizes: '32x32', file: 'favicon-32.png'}),
+  link({rel: 'icon', type: 'image/png', sizes: '48x48', file: 'favicon-48.png'}),
+  link({rel: 'icon', type: 'image/png', sizes: '96x96', file: 'favicon-96.png'}),
+  link({rel: 'icon', type: 'image/png', sizes: '144x144', file: 'favicon-144.png'}),
+  link({rel: 'icon', type: 'image/png', sizes: '192x192', file: 'favicon-192.png'}),
+  link({rel: 'apple-touch-icon', sizes:'120x120', file: 'touch-icon-iphone.png'}),
   link({rel: 'apple-touch-icon', sizes: '152x152', file: 'touch-icon-ipad.png'}),
   link({rel: 'apple-touch-icon', sizes: '167x167', file: 'touch-icon-ipad-retina.png'}),
   link({rel: 'apple-touch-icon', sizes: '180x180', file: 'touch-icon-iphone-retina.png'}),
-  link({rel: 'icon', sizes: '192x192', file: 'android-icon.png'}),
+  link({rel: 'icon', type: 'image/png', sizes: '192x192', file: 'android-icon.png'}),
   meta({name: 'msapplication-square310x310logo', file: 'microsoft-icon.png'})
 ]
