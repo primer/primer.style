352b9ac1711f09c4f41d9b9fd6eaa6048862003a
Emily Plummer <emplums@github.com>
2019-04-25 12:43:36 -0700

    deploy

--- pages/_error.js
+++ pages/_error.js
@@ -4,6 +4,7 @@ import {redirectTrailingSlash} from '../redirect'
 
 export default class Error extends React.Component {
   static getInitialProps(context) {
+    // redirect trailing slash URLs to "clean" ones without
     return redirectTrailingSlash(context) || getErrorProps(context)
   }
 
