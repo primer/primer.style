dfcf9c1332cb82417a9a5368968ec5a8638aa553
Emily Plummer <emplums@github.com>
2019-05-07 09:39:58 -0700

    add Component component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import App, {Container} from 'next/app'
 import styled, {ThemeProvider, createGlobalStyle} from 'styled-components'
-import {theme, BaseStyles} from '@primer/components'
+import {theme, BaseStyles, Box} from '@primer/components'
+import {Header, ResponsiveJumpNav, JumpNav} from '@primer/blueprints'
 import {initDraggables} from '../src/draggable'
 
 const GlobalStyle = createGlobalStyle`
@@ -20,6 +21,16 @@ const Anchor = styled.div`
 `
 
 export default class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
   componentDidMount() {
       initDraggables()
   }
@@ -29,6 +40,7 @@ export default class MyApp extends App {
   }
 
   render() {
+    const { Component, pageProps } = this.props;
     return (
       <BaseStyles fontSize={2} style={{fontFamily: theme.fonts.normal}}>
         <GlobalStyle />
@@ -38,7 +50,7 @@ export default class MyApp extends App {
               <JumpNav />
             </Header>
             <Container>
-              {children}
+              <Component {...pageProps} />
             </Container>
             <Anchor id="jumpnav" />
             <Box display={['block', 'block', 'block', 'none']}>
