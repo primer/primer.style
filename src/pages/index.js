9d02b4437a3ea667e0a06ac343375471e520b692
Shawn Allen <shawn.allen@github.com>
2019-07-25 14:43:12 -0700

    render the home page in <Layout>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import HiringPromo from '../components/HiringPromo'
 import PrimerCSS from '../components/PrimerCSS'
@@ -8,23 +9,19 @@ import OcticonsPromo from '../components/OcticonsPromo'
 import OpenSource from '../components/OpenSource'
 import PrimerReact from '../components/PrimerReact'
 
-function Index() {
-  return (
-    <>
-      <Hero />
-      <Divider my={[9, 12]} />
-      <HiringPromo />
-      <Divider my={[9, 12]} />
-      <PrimerCSS />
-      <Divider my={[9, 12]} />
-      <PrimerReact />
-      <Divider my={[9, 12]} />
-      <AtomPlugins />
-      <Divider my={[9, 12]} />
-      <OcticonsPromo />
-      <OpenSource />
-    </>
-  )
-}
-
-export default Index
+export default props => (
+  <Layout title="Home" {...props}>
+    <Hero />
+    <Divider my={[9, 12]} />
+    <HiringPromo />
+    <Divider my={[9, 12]} />
+    <PrimerCSS />
+    <Divider my={[9, 12]} />
+    <PrimerReact />
+    <Divider my={[9, 12]} />
+    <AtomPlugins />
+    <Divider my={[9, 12]} />
+    <OcticonsPromo />
+    <OpenSource />
+  </Layout>
+)
