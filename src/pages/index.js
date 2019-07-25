f8f46c93ef7840fe56930ca231aa19c62b150b30
Shawn Allen <shawn.allen@github.com>
2019-07-25 11:43:59 -0700

    bring back the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,30 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import HiringPromo from '../components/HiringPromo'
+import PrimerCSS from '../components/PrimerCSS'
+import Divider from '../components/Divider'
+import AtomPlugins from '../components/AtomPlugins'
+import OcticonsPromo from '../components/OcticonsPromo'
+import OpenSource from '../components/OpenSource'
+import PrimerReact from '../components/PrimerReact'
 
-export default function Index() {
-  return <>Hello, world!</>
+function Index() {
+  return (
+    <>
+      <Hero />
+      <Divider my={[9, 12]} />
+      <HiringPromo />
+      <Divider my={[9, 12]} />
+      <PrimerCSS />
+      <Divider my={[9, 12]} />
+      <PrimerReact />
+      <Divider my={[9, 12]} />
+      <AtomPlugins />
+      <Divider my={[9, 12]} />
+      <OcticonsPromo />
+      <OpenSource />
+    </>
+  )
 }
+
+export default Index
