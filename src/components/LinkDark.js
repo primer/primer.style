a4f73daf9ae3d05f703a7712ff7c051d184819db
Shawn Allen <shawn.allen@github.com>
2019-07-26 16:15:52 -0700

    nix duplicate display prop

--- src/components/LinkDark.js
+++ src/components/LinkDark.js
@@ -1,12 +1,10 @@
 // Extends Link from primer/components to make color primitives available. Ideally I'd use defaultProps here but because we use !important on utilities the theme colors won't override. We could probably add a function to handle this.
 
 import {Link, themeGet} from '@primer/components'
-import {display} from 'styled-system'
 import styled from 'styled-components'
 
 const LinkDark = styled(Link)`
   color: ${themeGet('colors.black')} !important;
-  ${display};
   &:hover {
     color: ${themeGet('colors.gray.8')};
   }
