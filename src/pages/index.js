1c2df90dc8b07e3d3705cd98b4372fd5fbc28083
Shawn Allen <shawn.allen@github.com>
2019-07-30 15:25:56 -0700

    add CSS, Components, Octicons, and Preso images

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import {BorderBox, Box, Flex, Heading, Text} from '@primer/components'
 import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Footer from '../components/Footer'
+import {ReactComponent as CSSImage} from '../svg/css.svg'
+import {ReactComponent as ComponentsImage} from '../svg/components.svg'
+import {ReactComponent as PresentationsImage} from '../svg/presentations.svg'
+import {ReactComponent as OcticonsImage} from '../svg/octicons.svg'
 
 const gutter = 5
 
@@ -19,16 +23,16 @@ export default function Index(props) {
             Check out our most popular tools to use in your next project:
           </Text>
           <Flex flexWrap="wrap" mr={[0, 0, -gutter]}>
-            <Promo title="CSS framework">
+            <Promo title="CSS framework" image={CSSImage}>
               Utility-centric and BEM-style components to give you the building blocks for any web project.
             </Promo>
-            <Promo title="React components">
+            <Promo title="React components" image={ComponentsImage}>
               Presentational UI components with encapsulated styles and constraint-based theme props.
             </Promo>
-            <Promo title="Octicons">
+            <Promo title="Octicons" image={OcticonsImage}>
               Your project. GitHub's icons. Available in Ruby and JavaScript implementations.
             </Promo>
-            <Promo title="Presentations">
+            <Promo title="Presentations" image={PresentationsImage}>
               Slide templates that let you focus on the story without having to worry about the design details.
             </Promo>
             <Promo title="Interface guidelines">
@@ -45,11 +49,13 @@ export default function Index(props) {
   )
 }
 
-function Promo({title, children, ...rest}) {
+function Promo({title, image: Image = 'div', children, ...rest}) {
   return (
     <Box pr={[0, 0, gutter]} mb={[6, 6, 4]} width={[1, 1, 1 / 2, 1 / 3]} {...rest}>
-      <BorderBox height={240} bg="gray.3" border={0} borderRadius={2} />
-      <Heading fontSize={4} mt={2}>
+      <BorderBox border={0} borderRadius={2} overflow="hidden">
+        <Box as={Image} bg="gray.3" width="100%" height={null} minHeight={220} />
+      </BorderBox>
+      <Heading fontSize={4} mt={1}>
         {title}
       </Heading>
       <Text as="p" m={0}>
