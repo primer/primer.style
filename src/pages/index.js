0bcae8766621a6e5cc4e0c12ec309f1bbefdb7fa
Shawn Allen <shawn.allen@github.com>
2019-07-30 14:50:35 -0700

    tweak spacing of home page items

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,21 @@ import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Footer from '../components/Footer'
 
+const gutter = 5
+
 export default function Index(props) {
   return (
     <Layout {...props}>
       <Hero />
       <Box bg="blue.1" color="black" px={4} py={12}>
-        <Box className="container-xl">
+        <Box className="container-xl" px={2}>
           <Heading fontSize={6} textAlign="center" lineHeight={1}>
             Everything in one system
           </Heading>
           <Text as="p" textAlign="center" mb={4}>
             Check out our most popular tools to use in your next project:
           </Text>
-          <Flex flexWrap="wrap">
+          <Flex flexWrap="wrap" mr={[0, 0, -gutter]}>
             <Promo title="CSS framework">
               Utility-centric and BEM-style components to give you the building blocks for any web project.
             </Promo>
@@ -45,7 +47,7 @@ export default function Index(props) {
 
 function Promo({title, children, ...rest}) {
   return (
-    <Box px={[0, 0, 3]} mb={[6, 6, 4]} width={[1, 1, 1 / 2, 1 / 3]} {...rest}>
+    <Box pr={[0, 0, gutter]} mb={[6, 6, 4]} width={[1, 1, 1 / 2, 1 / 3]} {...rest}>
       <BorderBox height={240} bg="gray.3" border={0} borderRadius={1} />
       <Heading fontSize={4} mt={2}>
         {title}
