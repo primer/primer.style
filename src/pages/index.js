912d6e6902faae625956623173cf134ae666b560
Shawn Allen <shawn.allen@github.com>
2019-07-30 15:57:46 -0700

    wrap promo elements in links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BorderBox, Box, Flex, Heading, Text} from '@primer/components'
+import {BorderBox, Box, Flex, Heading, Link, Text} from '@primer/components'
 import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Footer from '../components/Footer'
@@ -24,22 +24,22 @@ export default function Index(props) {
             Check out our most popular tools to use in your next project:
           </Text>
           <Flex flexWrap="wrap" mr={[0, 0, -gutter]}>
-            <Promo title="CSS framework" image={CSSImage}>
+            <Promo title="CSS framework" image={CSSImage} href="/css">
               Utility-centric and BEM-style components to give you the building blocks for any web project.
             </Promo>
-            <Promo title="React components" image={ComponentsImage}>
+            <Promo title="React components" image={ComponentsImage} href="/components">
               Presentational UI components with encapsulated styles and constraint-based theme props.
             </Promo>
-            <Promo title="Octicons" image={OcticonsImage}>
-              Your project. GitHub's icons. Available in Ruby and JavaScript implementations.
+            <Promo title="Octicons" image={OcticonsImage} href="https://octicons.github.com">
+              Your project. GitHub’s icons. Available in Ruby and JavaScript implementations.
             </Promo>
-            <Promo title="Presentations" image={PresentationsImage}>
+            <Promo title="Presentations" image={PresentationsImage} href="/presentations">
               Slide templates that let you focus on the story without having to worry about the design details.
             </Promo>
-            <Promo title="Interface guidelines" image={GuidelinesImage}>
+            <Promo title="Interface guidelines" image={GuidelinesImage} href="/design">
               Projects, standards, and usage guidelines for designing GitHub interfaces.
             </Promo>
-            <Promo title="Figma components">
+            <Promo title="Figma components" href="/figma">
               Design layouts, flows, wireframes, and make prototypes with Primer components in Figma.
             </Promo>
           </Flex>
@@ -50,16 +50,22 @@ export default function Index(props) {
   )
 }
 
-function Promo({title, image: Image = 'div', children, ...rest}) {
-  return (
-    <Box pr={[0, 0, gutter]} mb={[6, 6, 4]} width={[1, 1, 1 / 2, 1 / 3]} {...rest}>
-      <BorderBox as={Image} border={0} borderRadius={2} bg="gray.3" width="100%" height={null} minHeight={220} />
-      <Heading fontSize={4} mt={1}>
-        {title}
-      </Heading>
-      <Text as="p" m={0}>
-        {children}
-      </Text>
-    </Box>
-  )
+const A = props => <Link {...props} />
+A.defaultProps = {
+  color: 'inherit',
+  underline: false
 }
+
+const Promo = ({title, href, image: Image = 'div', children, ...rest}) => (
+  <Box pr={[0, 0, gutter]} mb={[6, 6, 4]} width={[1, 1, 1 / 2, 1 / 3]} {...rest}>
+    <A href={href} display="block" mb={1}>
+      <BorderBox as={Image} bg="black" border={0} borderRadius={2} width="100%" height={null} minHeight={220} />
+    </A>
+    <Heading fontSize={4}>
+      <A href={href}>{title}</A>
+    </Heading>
+    <Text as="p" m={0}>
+      {children}
+    </Text>
+  </Box>
+)
