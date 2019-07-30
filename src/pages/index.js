eda48f6b8a86a37abbf20dc7279746480746b2ee
Shawn Allen <shawn.allen@github.com>
2019-07-30 13:51:16 -0700

    stub out home page blocks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Box, Flex, Heading} from '@primer/components'
+import {Box, Flex, Heading, Text} from '@primer/components'
 import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Footer from '../components/Footer'
@@ -8,12 +8,51 @@ export default function Index(props) {
   return (
     <Layout {...props}>
       <Hero />
-      <Box bg="blue.1" color="black" py={12}>
-        <Heading fontSize={6} textAlign="center">
-          Everything in one system
-        </Heading>
+      <Box bg="blue.1" color="black" px={4} py={12}>
+        <Box className="container-xl">
+          <Heading fontSize={6} textAlign="center" lineHeight={1}>
+            Everything in one system
+          </Heading>
+          <Text as="p" textAlign="center" mb={4}>
+            Check out our most popular tools to use in your next project:
+          </Text>
+          <Flex flexWrap="wrap">
+            <Promo title="CSS framework">
+              Utility-centric and BEM-style components to give you the building blocks for any web project.
+            </Promo>
+            <Promo title="React components">
+              Presentational UI components with encapsulated styles and constraint-based theme props.
+            </Promo>
+            <Promo title="Octicons">
+              Your project. GitHub's icons. Available in Ruby and JavaScript implementations.
+            </Promo>
+            <Promo title="Presentations">
+              Slide templates that let you focus on the story without having to worry about the design details.
+            </Promo>
+            <Promo title="Interface guidelines">
+              Projects, standards, and usage guidelines for designing GitHub interfaces.
+            </Promo>
+            <Promo title="Figma components">
+              Design layouts, flows, wireframes, and make prototypes with Primer components in Figma.
+            </Promo>
+          </Flex>
+        </Box>
       </Box>
       <Footer />
     </Layout>
+  )
+}
+
+function Promo({image, title, children, ...rest}) {
+  return (
+    <Box px={[0, 0, 3]} mb={[6, 6, 4]} width={[1, 1, 1/2, 1/3]} {...rest}>
+      <Box height={200} bg="gray.3" />
+      <Heading fontSize={4} mt={2}>
+        {title}
+      </Heading>
+      <Text as="p" m={0}>
+        {children}
+      </Text>
+    </Box>
   )
 }
