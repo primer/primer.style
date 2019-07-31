5a63df1cb9bfababb5c31f43476d03a0c70be02a
Shawn Allen <shawn.allen@github.com>
2019-07-31 16:13:23 -0700

    add Header + support components

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {Flex, Link, Sticky, StyledOcticon} from '@primer/components'
+import {Link as GatsbyLink} from 'gatsby'
+import {MarkGithub} from '@primer/octicons-react'
+import NavDropdown from './NavDropdown'
+import NavItem from './NavItem'
+
+const NavLink = props => <Link color="blue.2" as={GatsbyLink} {...props} />
+const DropdownLink = props => <NavItem as={GatsbyLink} {...props} />
+
+export default function Header(props) {
+  return (
+    <Sticky {...props}>
+      <Flex bg="black" px={5} py={3} alignItems="center" justifyContent="space-between">
+        <Flex alignItems="center">
+          <StyledOcticon color="blue.4" icon={MarkGithub} size="medium" mr={3} />
+          <Link as={GatsbyLink} to="/" mr={4} color="blue.4" fontFamily="mono">
+            Primer
+          </Link>
+        </Flex>
+        <Flex as="nav" alignItems="center">
+          <NavLink to="/news">What’s new</NavLink>
+          <NavDropdown title="Design">
+            <DropdownLink to="/design">Interface guidelines</DropdownLink>
+            <DropdownLink to="https://octicons.github.com">Octicons</DropdownLink>
+            <DropdownLink to="/css/tools/prototyping">Prototyping</DropdownLink>
+          </NavDropdown>
+          <NavDropdown title="Development">
+            <DropdownLink to="/css">Primer CSS</DropdownLink>
+            <DropdownLink to="/components">Primer Components</DropdownLink>
+          </NavDropdown>
+          <NavLink to="/about">About</NavLink>
+        </Flex>
+      </Flex>
+    </Sticky>
+  )
+}
---
+++ src/components/NavDropdown.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import {ChevronDown} from '@githubprimer/octicons-react'
+import {Details, Text, Relative, StyledOcticon, Absolute, Box} from '@primer/components'
+// import getDirectionStyles from './getDirectionStyles'
+
+const DropdownMenu = styled.div`
+  box-shadow: 0px 4px 12px rgba(27, 31, 35, 0.15);
+  border: 1px solid rgba(219, 237, 255, 0.3);
+  border-radius: 4px;
+  max-width: 215px;
+  overflow: hidden;
+`
+
+export default function NavDropdown({children, title, color, direction = 'se', ...rest}) {
+  return (
+    <Box {...rest}>
+      <Details
+        overlay
+        mx={3}
+        render={({toggle}) => (
+          <>
+            <Text color={color} as="summary" onClick={toggle}>
+              {title} <StyledOcticon icon={ChevronDown} />
+            </Text>
+            <Relative>
+              <DropdownMenu as={Absolute} py={2} zIndex={90} bg="black" direction={direction}>
+                {children}
+              </DropdownMenu>
+            </Relative>
+          </>
+        )}
+      />
+    </Box>
+  )
+}
+
+NavDropdown.defaultProps = {
+  color: 'blue.2'
+}
+
+NavDropdown.propTypes = {
+  color: PropTypes.string,
+  direction: PropTypes.string,
+  title: PropTypes.string
+}
