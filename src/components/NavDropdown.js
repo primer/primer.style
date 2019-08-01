3d30cbd4677c454a9db721967f63fa3dc62553eb
Shawn Allen <shawn.allen@github.com>
2019-07-31 17:00:50 -0700

    fix octicons-react import

--- src/components/NavDropdown.js
+++ src/components/NavDropdown.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import {ChevronDown} from '@githubprimer/octicons-react'
+import {ChevronDown} from '@primer/octicons-react'
 import {Details, Text, Relative, StyledOcticon, Absolute, Box} from '@primer/components'
 // import getDirectionStyles from './getDirectionStyles'
 
