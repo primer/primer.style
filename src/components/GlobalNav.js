d62c292d711e312569c986f87048410c7faa488c
Shawn Allen <shawn.allen@github.com>
2019-08-05 10:36:48 -0700

    lint

--- src/components/GlobalNav.js
+++ src/components/GlobalNav.js
@@ -7,29 +7,31 @@ import NavItem from './NavItem'
 const NavLink = props => <Link color="blue.2" as={GatsbyLink} {...props} />
 
 export default function GlobalNav({navProps = {ml: 4}}) {
-  return <>
-    <NavLink to="/news" {...navProps}>
-      What’s new
-    </NavLink>
-    <NavDropdown title="Design" {...navProps}>
-      <NavItem as={GatsbyLink} to="/design">
-        Interface guidelines
-      </NavItem>
-      <NavItem href="https://octicons.github.com">Octicons</NavItem>
-      <NavItem as={GatsbyLink} to="/css/tools/prototyping">
-        Prototyping
-      </NavItem>
-    </NavDropdown>
-    <NavDropdown title="Development" {...navProps}>
-      <NavItem as={GatsbyLink} to="/css">
-        Primer CSS
-      </NavItem>
-      <NavItem as={GatsbyLink} to="/components">
-        Primer Components
-      </NavItem>
-    </NavDropdown>
-    <NavLink to="/about" {...navProps}>
-      About
-    </NavLink>
-  </>
+  return (
+    <>
+      <NavLink to="/news" {...navProps}>
+        What’s new
+      </NavLink>
+      <NavDropdown title="Design" {...navProps}>
+        <NavItem as={GatsbyLink} to="/design">
+          Interface guidelines
+        </NavItem>
+        <NavItem href="https://octicons.github.com">Octicons</NavItem>
+        <NavItem as={GatsbyLink} to="/css/tools/prototyping">
+          Prototyping
+        </NavItem>
+      </NavDropdown>
+      <NavDropdown title="Development" {...navProps}>
+        <NavItem as={GatsbyLink} to="/css">
+          Primer CSS
+        </NavItem>
+        <NavItem as={GatsbyLink} to="/components">
+          Primer Components
+        </NavItem>
+      </NavDropdown>
+      <NavLink to="/about" {...navProps}>
+        About
+      </NavLink>
+    </>
+  )
 }
