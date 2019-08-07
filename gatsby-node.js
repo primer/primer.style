d24f1a4a5a10553cf98c8ea80a01a1125e874825
Shawn Allen <shawn.allen@github.com>
2019-08-07 16:39:51 -0700

    lint

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const getReleases = require('@primer/releases')
 
 exports.createPages = ({actions: {createRedirect}}) => {
   for (const [fromPath, toPath] of Object.entries(redirects)) {
-    console.warn(`[redirect] ${fromPath} → ${toPath}`)
+    // console.warn(`[redirect] ${fromPath} → ${toPath}`)
     createRedirect({fromPath, toPath, redirectInBrowser: true})
   }
 }
