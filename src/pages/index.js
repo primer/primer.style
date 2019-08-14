dba3673667b1a0c0805877ea29d85ae57781ee30
Ash Guillaume <10384315+ashygee@users.noreply.github.com>
2019-08-14 15:58:32 -0700

    update interface guidelines description

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import {BorderBox, Box, Flex, Heading, Link, Text} from '@primer/components'
-import Hero from '../components/Hero'
-import Layout from '../components/Layout'
-import Footer from '../components/Footer'
-import {ReactComponent as CSSImage} from '../svg/css.svg'
-import {ReactComponent as ComponentsImage} from '../svg/components.svg'
-import {ReactComponent as PresentationsImage} from '../svg/presentations.svg'
-import {ReactComponent as OcticonsImage} from '../svg/octicons.svg'
-import {ReactComponent as GuidelinesImage} from '../svg/guidelines.svg'
-import {ReactComponent as FigmaImage} from '../svg/figma.svg'
+import React from "react";
+import { BorderBox, Box, Flex, Heading, Link, Text } from "@primer/components";
+import Hero from "../components/Hero";
+import Layout from "../components/Layout";
+import Footer from "../components/Footer";
+import { ReactComponent as CSSImage } from "../svg/css.svg";
+import { ReactComponent as ComponentsImage } from "../svg/components.svg";
+import { ReactComponent as PresentationsImage } from "../svg/presentations.svg";
+import { ReactComponent as OcticonsImage } from "../svg/octicons.svg";
+import { ReactComponent as GuidelinesImage } from "../svg/guidelines.svg";
+import { ReactComponent as FigmaImage } from "../svg/figma.svg";
 
-const gutter = 5
+const gutter = 5;
 
 export default function Index(props) {
   return (
@@ -26,41 +26,80 @@ export default function Index(props) {
           </Text>
           <Flex flexWrap="wrap" mr={[0, 0, -gutter]}>
             <Promo title="CSS framework" image={CSSImage} href="/css">
-              Utility-centric and BEM-style components to give you the building blocks for any web project.
+              Utility-centric and BEM-style components to give you the building
+              blocks for any web project.
             </Promo>
-            <Promo title="React components" image={ComponentsImage} href="/components">
-              Presentational UI components with encapsulated styles and constraint-based theme props.
+            <Promo
+              title="React components"
+              image={ComponentsImage}
+              href="/components"
+            >
+              Presentational UI components with encapsulated styles and
+              constraint-based theme props.
             </Promo>
-            <Promo title="Octicons" image={OcticonsImage} href="https://octicons.github.com">
-              Your project. GitHub’s icons. Available in Ruby and JavaScript implementations.
+            <Promo
+              title="Octicons"
+              image={OcticonsImage}
+              href="https://octicons.github.com"
+            >
+              Your project. GitHub’s icons. Available in Ruby and JavaScript
+              implementations.
             </Promo>
-            <Promo title="Presentations" image={PresentationsImage} href="/presentations">
-              Slide templates that let you focus on the story without having to worry about the design details.
+            <Promo
+              title="Presentations"
+              image={PresentationsImage}
+              href="/presentations"
+            >
+              Slide templates that let you focus on the story without having to
+              worry about the design details.
             </Promo>
-            <Promo title="Interface guidelines" image={GuidelinesImage} href="/design">
-              Projects, standards, and usage guidelines for designing GitHub interfaces.
+            <Promo
+              title="Interface guidelines"
+              image={GuidelinesImage}
+              href="/design"
+            >
+              Principles, standards, and usage guidelines for designing GitHub
+              interfaces.
             </Promo>
-            <Promo title="Figma components" image={FigmaImage} href="/design/tools/figma">
-              Design layouts, flows, wireframes, and make prototypes with Primer components in Figma.
+            <Promo
+              title="Figma components"
+              image={FigmaImage}
+              href="/design/tools/figma"
+            >
+              Design layouts, flows, wireframes, and make prototypes with Primer
+              components in Figma.
             </Promo>
           </Flex>
         </Box>
       </Box>
       <Footer />
     </Layout>
-  )
+  );
 }
 
-const A = props => <Link {...props} />
+const A = props => <Link {...props} />;
 A.defaultProps = {
-  color: 'inherit',
+  color: "inherit",
   underline: false
-}
+};
 
-const Promo = ({title, href, image: Image = 'div', children, ...rest}) => (
-  <Box pr={[0, 0, gutter]} mb={[6, 6, 8]} width={[1, 1, 1 / 2, 1 / 3]} {...rest}>
+const Promo = ({ title, href, image: Image = "div", children, ...rest }) => (
+  <Box
+    pr={[0, 0, gutter]}
+    mb={[6, 6, 8]}
+    width={[1, 1, 1 / 2, 1 / 3]}
+    {...rest}
+  >
     <A href={href} display="block" mb={1}>
-      <BorderBox as={Image} bg="black" border={0} borderRadius={2} width="100%" height={null} minHeight={220} />
+      <BorderBox
+        as={Image}
+        bg="black"
+        border={0}
+        borderRadius={2}
+        width="100%"
+        height={null}
+        minHeight={220}
+      />
     </A>
     <Heading fontSize={4}>
       <A href={href}>{title}</A>
@@ -69,4 +108,4 @@ const Promo = ({title, href, image: Image = 'div', children, ...rest}) => (
       {children}
     </Text>
   </Box>
-)
+);
