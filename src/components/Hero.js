e5bc5e2c59f16e3b81c08bb81ee16873941de635
Natalya Shelburne <natalyathree@gmail.com>
2021-06-04 10:04:55 -0400

    clearing hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -26,15 +26,6 @@ export default function Hero() {
             Primer was created for GitHub by GitHub. We love it so much, we chose to open-source it to allow the
             community to design and build their own projects with Primer.
           </Text>
-          <Text fontFamily="mono" as="p" color="blue.3" mt={5}>
-            <LinkLight fontSize={[0, 1, 2]} href="/about">
-              About
-            </LinkLight>
-            {DOT}
-            <LinkLight ml={2} fontSize={[0, 1, 2]} href="https://github.com/primer/">
-              Open-source
-            </LinkLight>
-          </Text>
         </Flex.Item>
       </IndexGrid>
     </Box>
