0a44ace815cda5f4a31f81d0f981a152391597fa
Josh Soref <jsoref@users.noreply.github.com>
2022-02-27 14:36:09 -0500

    spelling: kravetz
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/data/team.js
+++ src/data/team.js
@@ -476,7 +476,7 @@ Various design podcasts including [SimpleBits](https://show.simplebits.com/), [H
     cssFeature: 'word-break: break-word',
     questionOne: `Up until now, I've been mostly working Front End Engineering roles, but I've always been interested in finding something that was a good mix between Front End and Design. Design Systems feels like the perfect hybrid between the two! \n \n I'm also really into accessibility, and I found that having a design system that enforces accessible standards is one of the easiest ways to make progress on getting a team on-boarded with accessibility best practices and getting those practices implemented in production \n \n I'm super excited about our React component library that we've been working on over the past few months!`,
     questionTwo: `Some people & projects that have been inspiring to me are: are Nicole Sullivan, Vaidehi Joshi's [BaseCS blog](https://medium.com/basecs) &
-     , Mina Markham's [slay.sh](https://slay.sh/), Una Kravets, Suz Hinton's
+     , Mina Markham's [slay.sh](https://slay.sh/), Una Kravetz, Suz Hinton's
      [talk about accessibility at Dinosaur.js](https://www.youtube.com/watch?v=hKIQkgPVXH4), Marcy Sutton, and Anil Dash.`,
     favoriteTools: 'Figma, Notion (for taking notes & planning), React',
     avatar: 'https://user-images.githubusercontent.com/586552/46690198-b4fdc980-cbcf-11e8-95e3-c3e28b69a29c.jpg',
