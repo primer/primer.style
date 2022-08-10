ed98a19bd069c7932b3c469915d9e8df2ebe7fa6
Allie Thu <alliethu@github.com>
2022-08-10 10:02:23 -0600

    Update alliethu title and gif (#316)

--- src/data/team.js
+++ src/data/team.js
@@ -119,13 +119,13 @@ Various design podcasts including [SimpleBits](https://show.simplebits.com/), [H
   },
   {
     name: 'Allie Thu',
-    title: 'Systems Designer - Accessibility',
+    title: 'Accessibility Design Manager',
     handle: 'alliethu',
     questionOne: 'Early on in my career, I felt a natural pull towards design systems as I\'ve always been a person who finds great joy in consolidating, organizing, and making sense of messy things. As soon as I began to specialize in accessibility, the power and importance of design systems became even more clear, ultimately turning into the core of my work.',
     questionTwo: `I believe I inherited my curiousity and love for learning from my dad. My husband, who has helped teach me that I can be both a #bossatwork and a #bossmom. The Accessibility community who is small, but mighty, working hard to empower and educate people like me to grow the community even further. Lastly, all the open source design systems out there!`,
     favoriteTools: 'Notion, Slack, Figma, Microsoft Accessibility Insights, Spotify',
     avatar: 'https://user-images.githubusercontent.com/7228610/130680358-cd3a8b01-4fae-4e2c-ba05-e9ad6dd5cb14.jpg',
-    gif: 'https://user-images.githubusercontent.com/7228610/130678310-004fa38e-51ce-46f6-907f-ae9b7bc26e6c.gif',
+    gif: 'https://user-images.githubusercontent.com/7228610/183948245-6004efcf-1d90-40c0-8aba-22c4cdb6f7c1.gif',
     alumni: false
   },
   {
