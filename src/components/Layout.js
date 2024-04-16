ad8fba715d1996a4aa9240c5975b3bc92d4ba361
Daniel Adams <dipree@github.com>
2024-04-16 21:43:43 +0200

    Add custom head

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import {Box, ThemeProvider} from '@primer/react'
 import {Head, Header} from '@primer/gatsby-theme-doctocat'
-//import {Head} from './Head'
+import {CustomHead} from './CustomHead'
 import '@primer/css/layout/index.scss'
 import React from 'react'
 
@@ -8,6 +8,7 @@ export default function Layout({pageContext, children, colorMode}) {
   return (
     <ThemeProvider colorMode={colorMode || 'night'} nightScheme="dark_dimmed">
       <Head title={pageContext.frontmatter.title} description={pageContext.frontmatter.description} />
+      <CustomHead />
       <Header isSearchEnabled={false} />
       <Box as={'main'} sx={{backgroundColor: 'canvas.default', color: 'fg.default'}}>
         {children}
